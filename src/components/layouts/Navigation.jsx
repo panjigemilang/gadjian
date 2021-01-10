e62c9544d950da89b67727267a19b1818b35b999
panjigemilang <panjigemilang31298@gmail.com>
2021-01-10 20:59:45 +0700

    setup react and dependencies

---
+++ src/components/layouts/Navigation.jsx
@@ -0,0 +1,5 @@
+import React from "react"
+
+export default function Navigation() {
+  return <nav className="navigation-wrapper">Navbar</nav>
+}
---
+++ src/components/layouts/TopBar.jsx
@@ -0,0 +1,14 @@
+import React from "react"
+
+export default function TopBar() {
+  return (
+    <div className="topbar-wrapper">
+      <div className="user-wrapper">
+        <h1>
+          Hallo, <span>Gadjian User</span>
+        </h1>
+        <img src="" alt="" />
+      </div>
+    </div>
+  )
+}
